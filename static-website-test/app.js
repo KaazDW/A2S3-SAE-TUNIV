39c5d77f8fbef15f93f28593d336590065636c10
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-09-29 11:45:28 +0200

    psuh

---
+++ static-website-test/app.js
@@ -0,0 +1,47 @@
+var darkmod = true; console.log("var darkmod = " + darkmod);
+var ebody = document.getElementById("body");
+var eheader = document.getElementById("header");
+
+function ColorMod() {
+    if(darkmod){
+        eheader.classList.remove("header-dark"); 
+        eheader.classList.add("header-light");
+        ebody.classList.remove("body-dark"); 
+        ebody.classList.add("body-light");
+        darkmod = false;
+        console.log("light color-panel on");
+    }
+    else if(!darkmod){
+        eheader.classList.remove("header-light"); 
+        eheader.classList.add("header-dark");
+        ebody.classList.remove("body-light"); 
+        ebody.classList.add("body-dark");
+        darkmod = true;
+        console.log("dark color-panel on");
+
+    }
+}
+
+var LPO = false; console.log("var LPO = " + LPO); //Login Panel Open ?
+var elogin = document.getElementsByClassName("header-login-panel");
+
+function LoginPanel() {
+    if(!LPO){
+        elogin.style.display = 'block';
+        LPO = true;
+    }
+    else if(LPO){
+        elogin.style.display = 'none';
+        LPO = false;
+    }
+}
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+  // 👇️ hide button
+  btn.style.display = 'none';
+
+  // 👇️ show div
+  const box = document.getElementById('box');
+  box.style.display = 'block';
+});
