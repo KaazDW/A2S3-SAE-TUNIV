daf33b53ad55cdd919018ca75ae7432b9edc2103
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-10-12 12:11:09 +0200

    f

--- static-website-test/app.js
+++ static-website-test/app.js
@@ -45,3 +45,13 @@ btn.addEventListener('click', () => {
   const box = document.getElementById('box');
   box.style.display = 'block';
 });
+
+function checkboxLogin() {
+    var checkBox = document.getElementById("header-login");
+    var div = document.getElementById("header-login-panel");
+    if (checkBox.checked == true){
+      div.style.display = "block";
+    } else {
+       div.style.display = "none";
+    }
+  }
