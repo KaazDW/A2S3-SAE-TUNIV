cc722aade49aa4f90111c67951021da8038be47b
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-10-25 16:53:16 +0200

    +apropos_page

--- src/views/Apropos.vue
+++ src/views/Apropos.vue
@@ -1,20 +1,30 @@
 <template>
     <section class="apropos-page">
         <section>
+            <h1>🏆 TUNIV. <!--<span>- Tournament Handle Web Application</span>--></h1>
+            <p>Ce projet à été développé courant 2022 par un groupe de 3 étudiant dans le cadre de leur 2ème année de BUT Informatique au sein de l'IUT Lyon1 à Bourg-en-Bresse.</p>
             <div class="apropos-container-card">
                 <div class="apropos-card">
-                    <img src="../assets/ppempty.png"/>
-                    <h1>JOURNET<br><span>Gaël</span></h1>
+                    <a href="https://www.github.com/">
+                        <img src="../assets/ppempty.png"/>
+                        <h1>JOURNET<br><span>Gaël</span></h1>
+                    </a>
                 </div>
                 <div class="apropos-card">
-                    <img src="../assets/ppempty.png"/>
-                    <h1>MARCOURT<br><span>Jean-François</span></h1>
+                    <a href="https://www.github.com/kaazdw">
+                        <img src="../assets/ppempty.png"/>
+                        <h1>MARCOURT<br><span>Jean-François</span></h1>
+                    </a>
                 </div>
                 <div class="apropos-card">
-                    <img src="../assets/ppempty.png"/>
-                    <h1>OZIMEK<br><span>Nathan</span></h1>
+                    <a href="https://www.github.com/">
+                        <img src="../assets/ppempty.png"/>
+                        <h1>OZIMEK<br><span>Nathan</span></h1>
+                    </a>
                 </div>
             </div>
         </section>  
     </section>
 </template>
+
+<!-- fetch(url,get) -->
