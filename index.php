3100305770f7c09ab7b57d2e98d2e8bde218fc28
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-11-18 16:50:59 +0100

    push rework php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <?php include 'modules/head.php'; ?>
+</head>
+<body>
+    <!-- HEADER IMPORT MODULE -->
+    <?php include 'modules/header.php'; ?>
+    
+    <div class="margin-header"></div> <!-- issu with margin-top under the fixed header -->
+    <main class="main-index">
+        <h1>MAIN-PAGE</h1>
+       
+    </main>
+</body>
+<script src="app.js"></script>
+</html>
---
+++ modules/header.php
@@ -0,0 +1,23 @@
+<header>
+    <h1><a href="../index.php">TUNIV</a></h1>
+    <ul class="nav">
+        <li><a href="../pages/informations.php">Informations</a></li>
+        <li><a href="">A propos</a></li>
+        <p style="color: grey;">|</p>
+        <li><a href="">LOGIN</a></li>
+    </ul>
+    <div class="mobile-menu">
+        <input type="checkbox" id="mobile-menu-id" onclick="MobileMenu()">
+        <label for="mobile-menu-id">
+            <img src="../assets/img/menu-icon.png">
+        </label>
+    </div>
+    <div class="mobilemenu-closed">
+        <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+        </ul>
+    </div>
+</header>
