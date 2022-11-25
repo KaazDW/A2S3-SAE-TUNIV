60ac5645c7279b7525be4baa73e51576f1a3bfd0
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-11-25 12:19:14 +0100

    anim & apropos

--- pages/apropos.php
+++ pages/apropos.php
@@ -16,8 +16,33 @@ if (empty($_SESSION["admin"])) {
 
     <main class="main-apropos">
         <h2 class="title">A propos</h2>
-        <section>
-            
+        <section class="apropos-content">
+            <div class="apropos-title">
+                <h1>T</h1>
+                <h1>U</h1>
+                <h1>N</h1>
+                <h1>I</h1>
+                <h1>V</h1>
+                <h1>.</h1>
+            </div>    
+        <!-- <h1>TUNIV.</h1> -->
+            <p>TUNIV est une application WEB développé par 3 étudiants dans le cadre de leur 2ème Année de BUT informatique :</p>
+            <div class="card">
+                <div class="apropos-personnalcard">
+                    <img src="../assets/img/account.png">
+                    <a href="" target="_blank">Gael JOURNET</a>
+                </div>
+                <div class="apropos-personnalcard">
+                    <img src="../assets/img/account.png">
+                    <a href="http://github.com/kaazdw" target="_blank">Jean-François MARCOURT</a>
+                </div>
+                <div class="apropos-personnalcard">
+                    <img src="../assets/img/account.png">
+                    <a href="" target="_blank">Nathan Ozimek</a>
+                </div>
+            </div>
+            <p>L'objectif de l'application est de centraliser les différents tournois sportifs universitaire à l'aide d'un outil développé par des étudiants, pour des étudiants.</p>
+            <p>Tuniv à déjà hébergé <span>00</span> tournois depuis son déployement le <span>00.00.2022</span>.</p>
         </section>
     </main> 
 
