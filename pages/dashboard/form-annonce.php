7afb6cd0d9d806a2c10dcd70429e295d519529e4
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-12-13 08:43:24 +0100

    push

--- pages/dashboard/form-annonce.php
+++ pages/dashboard/form-annonce.php
@@ -17,43 +17,43 @@ if (!$_SESSION["type"]=="administrateur") {
 <body>
         <?php include '../../modules/header.php';?>
 
-    <main>
-        <h2 class="title">Créer une annonce</h2>
-        <section class="stats-section">
+    <main class="main-admin-annonce-form">
+        <h2 class="title">Générer une nouvelle annonce</h2>
+        <section>
+            <form action="../../config/config-annonce.php" method="POST" enctype="multipart/form-data">
+                    <!-- Champ titre -->
+                    <div class="form">
+                        <div class="form-parta">
+                            <label for="title">Titre de l'annonce</label>
+                            <input name="title" type="text" id="title" required="required">
 
-        <form action="../../config/config-annonce.php" method="POST" enctype="multipart/form-data">
-                <!-- Champ titre -->
-                <label for="title">Titre de l'annonce</label>
-                <input name="title" type="text" id="title" required="required">
-                <br>
+                            <!-- Champ auteur -->
+                            <label for="author">Auteur de l'annonce</label>
+                            <input name="author" type="text" id="author" required="required">
 
-                <!-- Champ auteur -->
-                <label for="author">Auteur de l'annonce</label>
-                <input name="author" type="text" id="author" required="required">
-                <br>
+                            <!-- Champ rôle -->
+                            <label for="role">Rôle de l'auteur</label>
+                            <input name="role" type="text" id="role" required="required">
+                            
+                            <!-- Champ image -->
+                            <label for="img">Image de l'annonce</label>
+                            <input name="img" type="file" id="img" accept="image/png, image/jpeg, image/avif" id="img">
+                        </div>
 
-                <!-- Champ rôle -->
-                <label for="role">Rôle de l'auteur</label>
-                <input name="role" type="text" id="role" required="required">
-                <br>
-
-                <!-- Champ contenu -->
-                <label for="content">Contenu de l'annonce</label>
-                <textarea placeholder="255 caractères maximum" name="content" id="content" cols="30" rows="8" maxlength="255"></textarea>
-                <br>
-
-                <!-- Champ image -->
-                <label for="img">Image de l'annonce</label>
-                <input name="img" type="file" id="img" accept="image/png, image/jpeg, image/avif" id="img">
-
-                <button type="submit">Créer l'annonce</button>
-        </form>
-
-        <?php if (!empty($_SESSION["annonceErreur"])) {
-            echo ("<p>$_SESSION[annonceErreur]</p>");
-            unset($_SESSION["annonceErreur"]);
-        } ?>
-
+                        <!-- Champ contenu -->
+                        <div class="textarea">
+                            <label for="content">Contenu de l'annonce</label>
+                            <textarea placeholder="255 caractères maximum" name="content" id="content" cols="30" rows="8" maxlength="255"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-button">
+                        <button type="submit">Créer l'annonce</button>
+                    </div>            
+            </form>
+            <?php if (!empty($_SESSION["annonceErreur"])) {
+                echo ("<p>$_SESSION[annonceErreur]</p>");
+                unset($_SESSION["annonceErreur"]);
+            } ?>
         </section>
     </main>
     <?php include '../../modules/footer.php'; ?>
