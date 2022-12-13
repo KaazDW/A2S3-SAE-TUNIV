bed8285ac35ee1a45b7fe7b17c8245360bfc1f67
Kaaz <92046563+KaazDW@users.noreply.github.com>
2022-12-13 10:49:21 +0100

    push

--- index.php
+++ index.php
@@ -15,31 +15,29 @@ $listeAnnonces = $pdo->query($sql);
 
 <body>
         <?php include 'modules/header.php'; ?>
-    <main class="main-home">
-        <section class="main-content">
-            <h1>TUNIV.</h1>
-            <a href="/pages/tournois.php">Accéder aux tournois</a>
+    <main>
+        <section class="main-home">
+            <section class="main-content">
+                <h1>TUNIV.</h1>
+                <a href="/pages/tournois.php">Accéder aux tournois</a>
+            </section>
         </section>
-
         <section class="index-actu">
-            <h2>Annonces</h2>
+            <h2>Actualités</h2>
 
             <?php $annonces = $listeAnnonces->fetchAll();
             foreach($annonces as $annonce):?>
-
-            <div class=bloc-annonce>
-                <h3><?php echo($annonce['Titre'])?></h3>
-                <img src=<?php if($annonce['Image']!=NULL) {echo($annonce['Image']);} else {echo("assets/img/pp-blanc.png");}?> alt="logo de l'annonce" >
-                <div class=date-annonce><?php echo($annonce['Date_annonce']) ?></div>
-                <div class=auteur-role-annonce><?php echo($annonce['Auteur']) ?>, <?php echo($annonce['Role'])?></div>
-                <div class=contenu-annonce><?php echo($annonce['Contenu'])?></div>
-            </div>
-
-            <?php endforeach;?>
+                <div class="annonce-card">
+                    <img src=<?php if($annonce['Image']!=NULL) {echo($annonce['Image']);} else {echo("assets/img/pp-blanc.png");}?> alt="logo de l'annonce" >
+                    <div class="text-field">
+                        <h3><?php echo($annonce['Titre'])?></h3>
+                        <p class="date"><?php echo($annonce['Date_annonce']) ?></p>
+                        <p><?php echo($annonce['Auteur']) ?>, <span class="role"><?php echo($annonce['Role'])?></span></p>
+                        <p><?php echo($annonce['Contenu'])?></p>
+                    </div>
+                </div>
+            <?php endforeach;?> 
         </section>
-
-            
-
     </main>
     <?php include 'modules/footer.php'; ?>
 </body>
