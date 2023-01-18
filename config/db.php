8ff54d78b1d5b6039b2fd575c5562a40cbe58d07
IUTABGDIN105-01\Administrateur <iutbourg.smir@univ-lyon1.fr>
2023-01-18 14:54:34 +0100

    Affichage des tournois (sans CSS)

--- config/db.php
+++ config/db.php
@@ -10,18 +10,18 @@
 //}
 
 // DB connection IUT
-try{
-    $pdo = new PDO ("mysql:dbname=p2106013;host=iutbg-lamp.univ-lyon1.fr", "p2106013", "12106013", [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-}catch(Exception $e){
-    die();
-}
-
-// DB connection IUT
-// try {
-//     $pdo = new PDO("mysql:dbname=p2106013;host=iutbg-lamp.univ-lyon1.fr", "p2106013", "12106013", [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
-// } catch (Exception $e) {
+// try{
+//     $pdo = new PDO ("mysql:dbname=p2106013;host=iutbg-lamp.univ-lyon1.fr", "p2106013", "12106013", [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
+// }catch(Exception $e){
 //     die();
 // }
+
+// DB connection IUT
+try {
+    $pdo = new PDO("mysql:dbname=p2106229;host=iutbg-lamp.univ-lyon1.fr", "p2106229", "12106229", [PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION]);
+} catch (Exception $e) {
+    die();
+}
 
     // $sql = "SELECT * FROM tuniv_db.utilisateurs;";
     // $db->exec($sql);
--- pages/match-tournois.php
+++ pages/match-tournois.php
@@ -29,11 +29,29 @@ $tournoi=$tournoi->fetchAll();
             <?php $nom = $tournoi[0]["Nom"];
             echo($nom);?>
         </h2>
+        <span><?php $etape = $tournoi[0]["Etape"];
+        if ($etape==0){
+            echo("Ce tournoi n'a pas encore commencé.");
+        }
+        else if ($etape==1){
+            echo("Ce tournoi est en cours.");
+        }
+        else {
+            echo("Ce tournoi est terminé.");
+        }
+        ?>
+        </span>
         <h3>Sport :</h3><span><?php $sport = $tournoi[0]["Sport"]; echo $sport;?></span>
         <h3>Date de début :</h3><span><?php $dateDeb = $tournoi[0]["DateDebut"]; echo $dateDeb;?></span>
         <h3>Date de fin :</h3><span><?php $dateFin = $tournoi[0]["DateFin"]; echo $dateFin;?></span>
+        <h3>Nombre d'équipes inscrites :</h3> <span>
+            <?php $sql = $pdo->prepare('SELECT COUNT(DISTINCT ID_Equipe) FROM Participer WHERE ID_Tournoi =:varId');
+            $sql->execute(['varId' =>$_GET["id"]]);
+            echo($sql->fetch()[0]);
+            ?>/<?php $nbMax = $tournoi[0]["Nb_Equipe"]; echo($nbMax);?>
+        </span>
 
-        <h2 class="title">Match</h2>
+        <h2 class="title">Matchs</h2>
         <?php
 
         foreach($matchs as $match):
