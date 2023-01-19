edbad1200b11c0139cdcc62c77b5460cf3aa3fd2
Kaaz <92046563+KaazDW@users.noreply.github.com>
2023-01-19 10:58:40 +0100

    correction code

--- modules/head.php
+++ modules/head.php
@@ -2,8 +2,8 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href="../assets/css/style.css" rel="stylesheet">
-<link href="../assets/scss/css/responsive.css" rel="stylesheet">
 <link href="../assets/scss/css/styleS.css" rel="stylesheet">
+<link href="../assets/scss/css/responsive.css" rel="stylesheet">
 <!-- <link href="../assets/scss/css/style.css" rel="stylesheet"> -->
 <title>TUNIV.</title>
 <script src="/app.js" defer></script>
