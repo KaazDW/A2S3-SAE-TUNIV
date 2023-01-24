f91c80cd0b5ffc23cbd889b1a94a0dd6dd16b741
IUTABGDIN105-01\Administrateur <iutbourg.smir@univ-lyon1.fr>
2023-01-24 14:43:54 +0100

    edit_user

--- pages/dashb-admin-editform-user.php
+++ pages/dashb-admin-editform-user.php
@@ -3,7 +3,7 @@ if (empty($_SESSION["type"])) {
     $_SESSION["type"] = false;
 }
 
-if ($_SESSION["type"]!="administrateur") {
+if ($_SESSION["type"] != "administrateur") {
     header("Location: ../index.php");
 }
 
@@ -11,83 +11,91 @@ include '../config/db.php';
 
 $user = $pdo->prepare('SELECT * FROM Utilisateurs where ID_User =:varId');
 
-$user->execute(['varId' =>$_GET["id"]]);
+$user->execute(['varId' => $_GET["id"]]);
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <?php include '../modules/head.php'; ?>
     <link href="../assets/css/style.css" rel="stylesheet">
-</head> 
+</head>
+
 <body>
-    <?php include '../modules/header.php';?>
+    <?php include '../modules/header.php'; ?>
     <main class="main-dashcap">
         <h2 class="title">Modifier l'utilisateur</h2>
         <section class="dashcap-section">
-            
+
             <section class="joueurs-liste-section">
                 <div class="joueurs-liste-div">
-                    
-<!-- Problème foreach et form -->
+
+                    <!-- Problème foreach et form -->
                     <?php
-                        $utilisateur = $pdo->prepare('SELECT Prenom, Nom from Utilisateur  where ID_User=:varId;');
-                        $utilisateur->execute(['varId' =>$_GET["id"]]);
-                        $joueurs=$utilisateur->fetchAll();
-                        foreach($joueurs as $joueur):    
+                    $utilisateur = $pdo->prepare('SELECT Nom,Prenom, Email, Type_user from Utilisateurs  where ID_User=:varId;');
+                    $utilisateur->execute(['varId' => $_GET["id"]]);
+                    $joueurs = $utilisateur->fetch();
+                    $nom= $joueurs[0];
+                    $prenom= $joueurs[1];
+                    $mail= $joueurs[2];
+                    $type= $joueurs[3];
                     ?>
 
-                    <?php
-                    endforeach;
-                    ?>
-                    
-                <div class="ajout-joueurs">
-                    <form action="../config/config-add-joueur-admin.php?id=<?php echo ($_GET['id'])?>" method='POST' enctype='multipart/form-data'>
-                        <h3>Ajouter un joueur</h3>
-                        <div>
-                            <label for="new-surname">Prénom</label>
-                            <input name="new-surname" id="new-surname">
-                        </div>
-                        <div>
-                            <label for="new-name">Nom</label>
-                            <input name="new-name" id="new-name">
-                        </div>
-                        <button>Valider</button>
-                    </form>
-                </div>
-                    
+                    <div class="ajout-joueurs">
+                        <form action="../config/config-admin-edit-user.php?id=<?php echo ($_GET["id"]) ?>" method='POST' enctype='multipart/form-data'>
+                            <h3>Modifier l'utilisateur</h3>
+                            <div>
+                                <label for="new-surname1">Prénom</label>
+                                <input name="new-surname1" value=<?php echo($prenom) ?> id="new-surname1">
+                            </div>
+                            <div>
+                                <label for="new-name1">Nom</label>
+                                <input name="new-name1" value=<?php echo($nom) ?> id="new-name1">
+                            </div>
+                            <div>
+                                <label for="new-mail1">Mail</label>
+                                <input name="new-mail1" value=<?php echo($mail) ?> id="new-mail1">
+                            </div>
+                            <div>
+                                <label for="new-type1">Type</label>
+                                <input name="new-type1" value=<?php echo($type) ?> id="new-type1">
+                            </div>
+                            <button>Valider</button>
+                        </form>
+                    </div>
 
 
+
                 </div>
-               
+
             </section>
             <section id="joueur-edit">
-                    <header>
-                        <button onclick="closeeditjoueurs()">Annuler</button>
-                    </header>
-                    <div class="content">
-                        <!-- CONTENU DU MENU D'EDITION D'UN JOUEURS -->
-                    </div>
+                <header>
+                    <button onclick="closeeditjoueurs()">Annuler</button>
+                </header>
+                <div class="content">
+                    <!-- CONTENU DU MENU D'EDITION D'UN JOUEURS -->
+                </div>
             </section>
         </section>
     </main>
     <?php include '../modules/footer.php'; ?>
     <script>
-
         // DISPLAY JOUEURS EDIT MENU
         document.getElementById('joueur-edit').style.display = "none";
 
-        function openeditjoueurs(){
-            document.getElementById('joueur-edit').style.display = "block";  
+        function openeditjoueurs() {
+            document.getElementById('joueur-edit').style.display = "block";
         }
 
-        function closeeditjoueurs(){
+        function closeeditjoueurs() {
             document.getElementById('joueur-edit').style.display = "none";
         }
-        document.getElementById('closebtneditjoueurs').addEventListener("click", function(){
+        document.getElementById('closebtneditjoueurs').addEventListener("click", function() {
             document.getElementById('joueur-edit').style.display = "none";
         });
-
     </script>
 </body>
+
 </html>
